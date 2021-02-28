56e1983b23bc05f67b54ffb07dcab24fe8e015d1
Matt <matt.furlong.12@gmail.com>
2021-02-28 09:32:46 -0500

    hid the api key and made minor css changes

---
+++ js/config.js
@@ -0,0 +1,5 @@
+var config = {
+    MY_KEY : '43d4ad0e5e266ed5d4afcf676d7bb122',
+    SECRET_KEY : '56789',
+    KEY_2 : '101010'
+  }
--- js/script.js
+++ js/script.js
@@ -1,39 +1,56 @@
-const searchInput = document.getElementById('city-input');
-const searchbutton = document.getElementById('search-button');
 const currentNameH2 = document.getElementById('weather-location');
 const currentTemperatureP = document.getElementById('weather-temperature')
 const currentWeatherP = document.getElementById('weather-weather');
 const currentResponseTime = document.getElementById('response-time');
+const displayWeatherDiv = document.getElementById('display-weather');
 
 async function displayWeather(cityName) {
-    let key = '61813c5f8cd90b489b39b0da474db348';
+    let key = config.MY_KEY;
     let currentTemperature;
     let currentWeather = "";
     let locationName = "";
     let start_time = new Date().getTime();
-
+    
+    // call the open weather map api
+    // THEN translate it to JSON
+    // THEN 
     let response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key)
     .then(function(response){ return response.json() })
     .then(function(data) {
+        //grab the data that I'm interested in ... get more from data variable ... do console.log(data)
         currentTemperature = data.main.temp;
         currentWeather = data.weather[0].main;
         locationName = data.name;
 
+        // manipulate data to prepare it for output
         let currentTemperatureCelcius = Math.round(parseFloat(currentTemperature)-273.15);
         let timeOfResponse = new Date().getTime() - start_time;
 
-        currentNameH2.innerHTML = 'City: ' + locationName;
-        currentTemperatureP.innerHTML = 'Temperature: ' + currentTemperatureCelcius;
+        //output data to the html elements/ output data to the user
+        displayWeatherDiv.style.display = "block";
+        currentNameH2.innerHTML = locationName;
+        currentTemperatureP.innerHTML = 'Temperature: ' + currentTemperatureCelcius + ' Degrees Celcius';
         currentWeatherP.innerHTML = 'Weather: ' + currentWeather;
         currentResponseTime.innerHTML = 'Response Time: ' + timeOfResponse + "ms";
-
-        console.table('Name: ', locationName, 'Weather :', currentWeather, 'Temperature', currentTemperatureCelcius, data);
     })
     .catch(function(error){
-        console.log(error)
+        displayWeatherDiv.style.display = "block";
+        currentNameH2.innerHTML = "No Data Found";
     });
 }
 
+function inputEntered(e){
+    if(e.key === 'Enter' || e.keyCode === 13) {
+        displayWeather(searchInput.value);
+    }
+    return;
+}
+
+
+const searchbutton = document.getElementById('search-button');
 searchbutton.addEventListener('click', () => {
     displayWeather(searchInput.value);
 });
+
+const searchInput = document.getElementById('city-input');
+searchInput.addEventListener('keyup', inputEntered);
